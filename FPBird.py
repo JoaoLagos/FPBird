d7c433e8ce817341b8bc4dd386d77571f3623d22
João Lagos <101952716+JoaoLagos@users.noreply.github.com>
2022-09-16 16:47:57 -0300

    Update

--- FPBird.py
+++ FPBird.py
@@ -1,22 +1,27 @@
 from PPlay.keyboard import *
 from PPlay.sprite import *
 from PPlay.window import *
+import random
 
 janela = Window(1280,720)
 janela.set_title("PF")
 teclado = Keyboard()
 objetos = []
 vidas = []
-passaro = Sprite("./coisas/imagens/bird.png")
-fundo = Sprite("./coisas/imagens/floresta.jpg")
+inimigos = []
+tiros = []
+passaro = Sprite("./coisas/imagens/Fly_1.png")
+fundo = Sprite("./coisas/imagens/cidade.jpg")
 passaro.x = 400
 passaro.y = janela.height/2 - passaro.height/2
-v = 300
-vp = 0
-pv = 500
+vObstaculo = 300
+vPassaro = 400
 rload = 15
 click = 0
 reloadDano = 0
+tiroReload = 15
+rSpawnInimigo = 0
+
 for i in range(0,3):
     vid = Sprite("./coisas/imagens/vida.png")
     vid.x = vid.width * i
@@ -27,37 +32,62 @@ while True:
     fundo.draw()
     reloadDano -= 5 * janela.delta_time()
     rload += 9 * janela.delta_time()
-    if teclado.key_pressed("space") and click == 0:
-        vp = -200
-    if vp < 200:
-        vp += 300 * janela.delta_time()
-    #passaro.y += vp * janela.delta_time()
+    rSpawnInimigo += 9 * janela.delta_time()
+
     if teclado.key_pressed("w") and passaro.y > 0:
-        passaro.y -= pv * janela.delta_time()
+        passaro.y -= vPassaro * janela.delta_time()
     if teclado.key_pressed("s") and passaro.y < 720:
-        passaro.y +=pv * janela.delta_time()
-    if teclado.key_pressed("d") and passaro.x + passaro.width < 1280:
-       passaro.x += pv * janela.delta_time()
+        passaro.y += vPassaro * janela.delta_time()
+    if teclado.key_pressed("d") and passaro.x + passaro.width < janela.width/2:
+       passaro.x += vPassaro * janela.delta_time()
     if teclado.key_pressed("a") and passaro.x > 0:
-        passaro.x -= pv * janela.delta_time()
-    if rload > 15:
-        obj = Sprite("./coisas/imagens/pad2.png")
+        passaro.x -= vPassaro * janela.delta_time()
+
+    tiroReload += 10 * janela.delta_time()
+    if teclado.key_pressed("space") and tiroReload >= 5:
+        tiro = Sprite("./coisas/imagens/Bullet_1.png")
+        tiro.y = passaro.y + passaro.height/2 - tiro.height/2
+        tiro.x = passaro.x + tiro.width
+        tiros.append(tiro)
+        tiroReload = 0
+    if len(tiros) > 0:
+        for t in tiros:
+            t.x += 500 * janela.delta_time()
+            t.draw()
+            if t.x >= 1280:
+                tiros.remove(t)
+
+    if rload > 10:
+        posx = random.randint(1,10)
+        obj = Sprite("./coisas/imagens/bola.png")
         obj.x = janela.width
+        obj.y = obj.height * posx
         objetos.append(obj)
-        obj2 = Sprite("./coisas/imagens/pad2.png")
-        obj2.y = janela.height - obj.height
-        obj2.x = janela.width
-        objetos.append(obj2)
         rload = 0
+
     else:
         for o in objetos:
-            if passaro.collided(o) and reloadDano < 0:
+            if passaro.collided(o) and reloadDano < 0 and len(vidas) != 0:
                 vidas.remove(vidas[len(vidas) - 1])
+                objetos.remove(o)
                 reloadDano = 5
-            o.x -= v * janela.delta_time()
+            o.x -= vObstaculo * janela.delta_time()
             o.draw()
             if o.x <= - o.width:
                 objetos.remove(o)
+
+    if rSpawnInimigo > 14 and len(inimigos) < 3:
+        inimigo = Sprite("./coisas/imagens/inimigo1.png")
+        inimigo.x = janela.width
+        inimigo.y = random.randint(1,5) * inimigo.height + 20
+        inimigos.append(inimigo)
+        rSpawnInimigo = 0
+    if len(inimigos) > 0:
+        for ini in inimigos:
+            if ini.x > 1100:
+                ini.x -= 100 * janela.delta_time()
+            ini.draw()
+
     passaro.draw()
     for vida in vidas:
         vida.draw()
