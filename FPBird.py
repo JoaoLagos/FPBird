779b3e4d0a4252c2bfb5f45b5533eaeb75d9b8c1
Majoetom <64480751+Majoetom@users.noreply.github.com>
2022-09-23 15:43:14 -0300

    Algumas alterações
    Atualizei o menu, mudei a velocidade de algumas coisas e deixei os cenarios se movendo

--- FPBird.py
+++ FPBird.py
@@ -8,12 +8,15 @@ def gameplay():
     cenario1 = "components/background/bg1_cidade.jpg"
     cenario2 = "components/background/bg2_colinas.jpg"
     cenario3 = "components/background/bg3_floresta.jpg"
-    cenario = [cenario1, cenario2, cenario3]
-    fundo = GameImage(cenario[random.randint(0, 2)])
+    cenarios = [cenario1, cenario2, cenario3]
+    cenario = cenarios[random.randint(0, 2)]
+    fundo = GameImage(cenario)
+    fundo2 = GameImage(str(cenario).strip(".jpg")+"_Inverso.jpg")
+    fundo2.x = janela.width
 
-    #fundo = Sprite("components/background/Cidade.jpg") #foi uma ideai q eu tive para fazer a movimentação
-    #fundo2 = Sprite("components/background/Cidade.jpg")
-    #fundo2.x = janela.width * 2
+    rNuvem = 7
+    nuvems = []
+
 
     objetos = []
     vidas = []
@@ -33,7 +36,7 @@ def gameplay():
         vidas.append(vid)
 
 # Variáveis
-    vObstaculo = 300
+    vObstaculo = 500
     vPassaro = 400
     rload = 15
     click = 0
@@ -45,20 +48,48 @@ def gameplay():
 # Gameloop
     while len(vidas) > 0:
 
+        rNuvem += 3 * janela.delta_time()
+
+        fundo.x -= 200 * janela.delta_time()
+        fundo2.x -= 200 * janela.delta_time()
+        if fundo.x <= 0 - fundo.width:
+            fundo.x = janela.width
+        if fundo2.x <= 0 - fundo2.width:
+            fundo2.x = janela.width
+
         fundo.draw()
+        fundo2.draw()
         reloadDano -= 5 * janela.delta_time()
         rload += 9 * janela.delta_time()
         rSpawnInimigo += 9 * janela.delta_time()
+
+
 
         # Entradas
         if teclado.key_pressed("w") and passaro.y > 0:
             passaro.y -= vPassaro * janela.delta_time()
         if teclado.key_pressed("s") and passaro.y < 720:
             passaro.y += vPassaro * janela.delta_time()
-        if teclado.key_pressed("d") and passaro.x + passaro.width < janela.width / 2:
+        if teclado.key_pressed("d") and passaro.x < janela.width / 2:
             passaro.x += vPassaro * janela.delta_time()
         if teclado.key_pressed("a") and passaro.x > 0:
             passaro.x -= vPassaro * janela.delta_time()
+
+        # Spawna nuvens###########
+        if rNuvem >= 7:
+            rNuvem = 0
+            nuvem = Sprite("components/background/nuvem.png")
+            nuvem.x = janela.width
+            nuvem.y = 100 * random.randint(0, 11)
+            nuvems.append(nuvem)
+        if len(nuvems) > 0:
+            for nuv in nuvems:
+                nuv.x -= 300 * janela.delta_time()
+                if nuv.x < 0 - nuv.width:
+                    nuvems.remove(nuv)
+                nuv.draw()
+        #########################
+
 
         ## Entrada dos tiros e sua movimentação
         tiroReload += 10 * janela.delta_time()
@@ -145,14 +176,79 @@ def gameplay():
             janela.clear()
 
         janela.update()
-    return 0
+    gameOver = Sprite("components/sprites/game_over.png")
+    gameOver.x = janela.width/2 - gameOver.width/2
+    gameOver.y = janela.height/2 - gameOver.height/2
+    while True:
+        playerX = passaro.x
+        playery = passaro.y
+        passaro = Sprite("components/sprites/plane/Dead_1.png")
+        passaro.x = playerX
+        passaro.y = playery
+
+        fundo.draw()
+        fundo2.draw()
+        passaro.draw()
+        for nuv in nuvems:
+            nuv.draw()
+        for ini in inimigos:
+            ini.draw()
+        for tiro in tiros:
+            tiro.draw()
+        for obj in objetos:
+            obj.draw()
+        gameOver.draw()
+        janela.draw_text(str(pontos), 20, 20, size=40, bold=True)
+        janela.update()
+        if teclado.key_pressed("esc"):
+            return 0
 
 def menu():
-    fundo = GameImage("./components/background/fundoespaco.jpg")
+    aviao = Sprite("components/sprites/plane/Fly_1.png")
+    aviao.x = janela.width/2 - aviao.width/2
+    aviao.y = janela.height/2 - aviao.height/2
+    fundo = GameImage("components/background/bg2_colinas.jpg")
+    fundo2 = GameImage("components/background/bg2_colinas_Inverso.jpg")
+    txt = Sprite("components/background/Texto.png")
+    txt.x = janela.width/2 - txt.width/2
+    txt.y = janela.height/4 - txt.height/2
+    fundo2.x = janela.width
+    vMenu = 70
+    rNuvem = 7
+    nuvems = []
     while True:
+        fundo.x -= 50 * janela.delta_time()
+        fundo2.x -= 50 * janela.delta_time()
+        if fundo.x <= 0 - fundo.width:
+            fundo.x = janela.width
+        if fundo2.x <= 0 - fundo2.width:
+            fundo2.x = janela.width
+
+        if aviao.y >= janela.height/2 + 70:
+            vMenu = - abs(vMenu)
+        if aviao.y <= janela.height/2 - 70:
+            vMenu = + abs(vMenu)
+        aviao.y += vMenu * janela.delta_time()
+
         fundo.draw()
+        fundo2.draw()
+        aviao.draw()
+        rNuvem += 2 * janela.delta_time()
+        if rNuvem >= 7:
+            rNuvem = 0
+            nuvem = Sprite("components/background/nuvem.png")
+            nuvem.x = janela.width
+            nuvem.y = 100 * random.randint(0, 11)
+            nuvems.append(nuvem)
+        if len(nuvems) > 0:
+            for nuv in nuvems:
+                nuv.x -= 200 * janela.delta_time()
+                if nuv.x < 0 - nuv.width:
+                    nuvems.remove(nuv)
+                nuv.draw()
+        txt.draw()
         janela.update()
-        if teclado.key_pressed("space"):
+        if teclado.key_pressed("enter"):
             return 1
 
 # Inicio
